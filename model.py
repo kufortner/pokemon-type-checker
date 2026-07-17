a0b9970699842f44ef168e231a5693f380a065ca
kufortner <kufortner@gmail.com>
2026-07-16 21:10:29 -0500

    Create initial model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+"""
+Pokémon Type Matchup Checker
+
+Version: 1.0.0
+
+This module will eventually contain the type matchup logic.
+"""
+
+
+def get_matchup():
+    """
+    Placeholder function for future matchup logic.
+    """
+    return "Model is working!"
