b697def9e6297842769f9d40b41039e0d9087e06
Kyle Fortner <kufortner@gmail.com>
2026-07-23 21:42:46 -0500

    Add interactive Matplotlib chart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 st.title("Pokémon Type Checker")
 selected_type = st.selectbox(
@@ -6,3 +7,18 @@ selected_type = st.selectbox(
     ["Fire", "Water", "Grass"],
 )
 st.write("You selected:", selected_type)
+
+effectiveness = {
+    "Fire": [2.0, 1.0, 0.5, 0.5],
+    "Water": [0.5, 1.0, 2.0, 2.0],
+    "Grass": [0.5, 1.0, 0.5, 2.0],
+}
+
+fig, ax = plt.subplots()
+ax.bar(
+    ["Grass", "Normal", "Water", "Fire"],
+    effectiveness[selected_type],
+)
+ax.set_title(f"{selected_type} Attack Effectiveness")
+ax.set_ylabel("Damage multiplier")
+st.pyplot(fig)
